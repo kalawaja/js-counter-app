e947ae49ee241079a4bd22293d77e6b975b315d6
Ramazan İLTER <remzilter@gmail.com>
2022-12-05 01:41:34 +0300

    long solution without functions

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let add = document.querySelector("#add");
+let substract = document.querySelector("#substract");
+
+console.log(add, substract);
+
+add.addEventListener("click", function() {
+    let output = document.querySelector("#output");
+    let result = Number(output.innerText) + 1;
+
+    if (result > 10) {
+        result = 0;
+    }
+    
+    output.innerText = result;
+});
+
+substract.addEventListener("click", function() {
+    let output = document.querySelector("#output");
+    let result = Number(output.innerText) -1;
+
+    if (result < 0) {
+        result = 0;
+    }
+
+    output.innerText = result;
+});
+
