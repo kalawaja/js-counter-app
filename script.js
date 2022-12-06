ce5d1ec28a5f011ba5f2239a7e0302ac19b755a4
Ramazan İLTER <remzilter@gmail.com>
2022-12-07 01:00:02 +0300

    cleaned the excess code

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-console.log(btns);
-
 btns.forEach(function(btn) {
     btn.addEventListener("click", function(e) {
         const styles = e.currentTarget.classList;
